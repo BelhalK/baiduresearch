86c8ed2d470067e8c6f20b84d4efd0842cd09c42
BelhalK <belhal.karimi@gmail.com>
2020-03-30 01:15:18 +0200

    maj

--- 3)SAEM/DeformableTemplate/test_run_usps.R
+++ 3)SAEM/DeformableTemplate/test_run_usps.R
@@ -20,46 +20,97 @@ data(digits) #import USPS digits dataabse
 image(matrix(rexp(256, rate=.1), ncol=16))
 
 N <- 5  # number of images
-dataset = digits[,3000:(3000+N)]
+images = digits[,3000:(3000+N)]
+
 for (i in 1:N){
-  sample.digit = matrix(dataset[,i], ncol=16,byrow=FALSE)  
+  sample.digit = matrix(images[,i], ncol=16,byrow=FALSE)  
   image(t(sample.digit)[,nrow(sample.digit):1])
 }
+
 
 # sample.digit = matrix(digits[,3000], ncol=16,byrow=FALSE)
 # image(t(sample.digit)[,nrow(sample.digit):1])
 
 
-template.model<-function(z, xi,id,xidep) { 
-  zi<-psi[id,]
-  xii <- xi[i,]
-  
-  deformation <- kernel.deformation %*% zi
-  template <- kernel.template %*% xii
+#Hyperparam
+p <- ncol(sample.digit) #dimension of the input
+kp <- 5 #dimension of the parameter of the template
+kg <- 5 #dimension of the random effects
+Gamma.star <- diag(rep(1,kg)) # covariance
 
-  ypred<- template
+
+
+
+phi <- as.list(numeric(16^2))
+dim(phi) <- c(16,16)
+
+# Assignment per element:
+phi[[1,1]] <- 1:4
+phi[[1,2]] <- 1:10
+phi[[2,1]] <- "phi"
+phi[[2,2]] <- c(1,2)
+
+
+chol.omega.z<-try(chol(Gamma.star))
+z1 <- matrix(rnorm(2*kg),ncol=kg)%*%chol.omega.z
+z <- list(z1, z1) #random effects (2 X kg)
+
+xi = matrix(NA,nrow=kp,ncol=1) #template fixed parameters (1 X kp)
+
+landmarks.p = matrix(rnorm(2*kp),ncol=kp)
+landmarks.g = matrix(rnorm(2*kg),ncol=kg)
+
+
+##TEST OUTSIDE THE FUNCTION 
+m = 2
+j=3
+x.ind = 2*m/p-1
+y.ind = 2*j/p-1
+rep.coord = matrix(c(x.ind,y.ind), nrow=1)
+coord <- t(apply(rep.coord, 2, rep, kg))
+diff = coord - landmarks.g
+
+kernel.deformation = exp(-(coord - landmarks.g)^2/2)
+colSums(kernel.deformation)%*%t(z[[1]])
+kernel.deformation
+##TEST OUTSIDE THE FUNCTION 
+
+
+
+template.model<-function(image, z, xi,id,xidep,p) { 
+  zi<-z[[id]]
+  
+  kernel.g = matrix(NA,nrow=1,ncol=p)
+  ypred = matrix(NA,nrow=p,ncol=p)
+
+  phi <- as.list(numeric(p^2))
+  dim(phi) <- c(p,p)
+  
+  sigma.g = 1
+  sigma.p = 1
+
+  for (m in 1:p){
+  	for (j in 1:p){
+  		x.ind = 2*m/p-1
+  		y.ind = 2*j/p-1
+  		rep.coord = matrix(c(x.ind,y.ind), nrow=1)
+		coord <- t(apply(rep.coord, 2, rep, kg))
+  		
+  		kernel.deformation = exp(-(coord - landmarks.g)^2/(2*sigma.g))
+		phi[[i,j]]= colSums(kernel.deformation)%*%t(zi)
+  		
+  		coord.template = coord - phi[[i,j]]
+  		kernel.template = exp(-(coord.template - landmarks.p)^2/(2*sigma.p))
+
+  		template = colSums(kernel.template)%*%t(xi)
+
+  		ypred[i,j] = template
+  	}
+
+  } 
 
   return(ypred)
 }
-
-
-#Hyperparam
-p <- ncol(sample.digit) #dimension of the input
-k_p <- 5 #dimension of the parameter of the template
-k_g <- 5 #dimension of the random effects
-Gamma.star <- diag(rep(1,p)) # covariance
-
-
-# mu.star <- rep(0,p)  # mean of the explanatory variables
-# Sigma.star <- diag(rep(1,p)) # covariance
-# beta.star <- c(1, 1,  0) # coefficients
-# beta0.star <- 0 # intercept
-# beta.true = c(beta0.star,beta.star)
-# X.complete <- matrix(rnorm(N*p), nrow=N)%*%chol(Sigma.star) +
-#               matrix(rep(mu.star,N), nrow=N, byrow = TRUE)
-# p1 <- 1/(1+exp(-X.complete%*%beta.star-beta0.star))
-# y <- as.numeric(runif(N)<p1)
-
 
 
 batchsize = 1
@@ -67,7 +118,8 @@ nb.epochs <-10
 nb.iter <- N/batchsize*nb.epochs
 
 # SAEM
-fit.params = miss.saem(X.obs,y,maxruns=nb.iter,k1=0,ll_obs_cal=FALSE, algo = "saem")
+fit.params = miss.saem(images,template.model,maxruns=nb.iter,k1=0,ll_obs_cal=FALSE, algo = "saem")
+
 
 #MCEM
 # list.mcem = miss.saem(X.obs,y,maxruns=nb.iter,ll_obs_cal=FALSE, algo = "mcem")
