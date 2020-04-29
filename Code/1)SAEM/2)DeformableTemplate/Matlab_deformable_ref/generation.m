5316a0d4349c904731ba8436084208d011c2b475
BelhalK <belhal.karimi@gmail.com>
2020-04-29 11:27:04 +0200

    maj

--- Code/1)SAEM/2)DeformableTemplate/Matlab_deformable_ref/generation.m
+++ Code/1)SAEM/2)DeformableTemplate/Matlab_deformable_ref/generation.m
@@ -1,4 +1,8 @@
-modeltest = load("tmp_new/model1.mat");
+%onlineref = load("tmp_new/online_ref.mat");
+%dispTemplate(onlineref.model,500,1)
+
+%onlinescalaire= load("tmp_new/modelOnlineScalaireRigide.mat"); 
+%dispTemplate(onlinescalaire.modelOnlineScalaireRigide.model,500,1)
 
 online = load("tmp_new/tmp_online.mat");
 full = load("tmp_new/tmp_batch.mat");
@@ -7,24 +11,21 @@ vr = load("tmp_new/tmp_vr.mat");
 fi = load("tmp_new/tmp_fi.mat");
 
 it = 30;
-flag_fig =1;
 %iter = [3,10,20,25,30,40];
 %iter = [3,10,20,50, 80, 90];
-iter = [1,2,3,4,5,6,10,20,40,60,80,99];
+%iter = [1,2,3,4,5,6,10,20,40,60,80,99];
+iter = [1,2,3,4,5,6,10,20];
 
-%display inference
+dispTempIter(fi.model,iter,1)
 
+
+%display All Algos inference
 %models = [full,online,incremental,vr,fi];
-%dispTempAll(models,iter,flag_fig)
-
-
-dispTemplate(modeltest.model,500,flag_fig)
-
-%disp = fi.model;
-%dispTempIter(disp,iter,flag_fig)
+%dispTempAll(models,iter,1)
 
 %Various parameters convergence plots
+%disp = fi.model;
 %convergence_weight(disp,it)
-%convergence_sigma(disp,it,flag_fig)
-%convergence_gamma(disp,it,flag_fig)
+%convergence_sigma(disp,it,1)
+%convergence_gamma(disp,it,1)
 
