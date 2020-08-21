10ba9b575519e87f234ad38df9abd2ad94fc69aa
BelhalK <belhal.karimi@gmail.com>
2020-08-21 18:11:01 +0200

    maj

--- Code/d)FederatedLearningSketch/CodeFarzinOldPaper/pcode/components/datasets/loader/epsilon_or_rcv1_folder.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pcode.tracking.logging import log
-from pcode.components.datasets.loader.utils import IMDBPT
-
-
-def define_epsilon_or_rcv1_or_MSD_folder(args,root):
-    log('load epsilon_or_rcv1_or_MSD from lmdb: {}.'.format(root), args.debug)
-    return IMDBPT(root, is_image=False)
--- Code/d)FederatedLearningSketch/CodeFarzinOldPaper/pcode/components/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .convex.least_square import *
-from .convex.logist_regression import *
-
-from .nonconvex.resnet import *
-from .nonconvex.densenet import *
-from .nonconvex.wideresnet import *
-from .nonconvex.mlp import *
-from .nonconvex.cnn import *
