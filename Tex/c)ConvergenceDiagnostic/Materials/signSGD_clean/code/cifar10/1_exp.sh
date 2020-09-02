f3e2c0b1286b97ad49271068897b53026f23084b
BelhalK <belhal.karimi@gmail.com>
2020-09-02 12:11:05 +0200

    maj

--- Tex/c)ConvergenceDiagnostic/Materials/signSGD_clean/code/cifar10/1_exp.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# normal run to see how good signSGD does
-ipython main.py -- --dataset cifar10 --epochs 150 --schedule 80 120 --lr 0.001 --momentum 0 --weight_decay 0 --optimizer signSGD --gpu 7 6 --logname cifar.resnet.sign1.072319
