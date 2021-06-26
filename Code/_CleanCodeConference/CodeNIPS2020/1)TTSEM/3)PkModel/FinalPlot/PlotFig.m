e242acd7994754abf26f42b1c3619634a7e499aa
BelhalK <belhal.karimi@gmail.com>
2021-06-25 18:20:16 -0700

    maj

--- Code/_CleanCodeConference/CodeNIPS2020/1)TTSEM/3)PkModel/FinalPlot/PlotFig.m
+++ Code/_CleanCodeConference/CodeNIPS2020/1)TTSEM/3)PkModel/FinalPlot/PlotFig.m
@@ -5,21 +5,21 @@ load data.txt;
 
 len = 1e5+1; 
 
-saem = data(1:30,2);
-isaem = data(31:60,2);
-vrsaem = data(61:90,2);
-fisaem = data(91:120,2);
+saem = data(2:32,2);
+isaem = data(33:63,2);
+vrsaem = data(64:94,2);
+fisaem= data(95:125,2);
 
-x = 0:29; 
+x = 0:30; 
 
 figure;box on; grid on; 
 semilogy(x,isaem,'b-','linewidth',2);hold on; grid on;
-plot(x,vrsaem,'k-','linewidth',2);hold on; grid on;
 plot(x,fisaem,'g-','linewidth',2);hold on; grid on;  
+plot(x,vrsaem,'k-','linewidth',2);hold on; grid on;
 plot(x,saem,'r-','linewidth',2);hold on; grid on;
 set(gca,'fontsize',20);
 xlabel('Epoch');
-ylabel('|k_a - k_a^*|^2');
-legend('iSAEM','fiSAEM','vrSAEM','SAEM')
+ylabel('|ka - ka^*|^2');
+legend('iSAEM','fiTTEM','vrTTEM','SAEM')
 set(gca,'yminorgrid','off');
-set(gca,'xtick',[0:10:20]);
+set(gca,'xtick',[0:10:30]);
